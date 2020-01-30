40fad5e3570696197f9f68ebbb7a2a25a7c66693
ZeProf2C <mail@normanalie.fr>
2020-01-30 16:43:43 +0400

    2 30/01/2020 - Batterie
    Plus de batterie

--- Obstacle.lua
+++ Obstacle.lua
@@ -10,6 +10,8 @@ obstacle.new = function(Xmap, Ymap, lenght, dir, image, name)
         Obstacle.lenght = lenght or 1 --Longueur de l'obstalce en blocs
         Obstacle.dir = dir or 1       --Direction 1: X croissant, 2: X décroissant, 3: Y croissant, 4: osjoxcbn
         Obstacle.name = name
+        Obstacle.Xlenght = nil
+        Obstacle.Ylenght = nil
         Obstacle.image = {}
             Obstacle.image.src   = love.graphics.newImage(image)
             Obstacle.image.width = Obstacle.image.src:getWidth()
@@ -18,44 +20,53 @@ obstacle.new = function(Xmap, Ymap, lenght, dir, image, name)
 
         Obstacle.update = function(map) --Prends un objet map et calcul les coords reels de l'obstacle. Rends .busy ses positions. Ne retourne rien
             if Obstacle.dir == 1 then
-                Obstacle.XmapEnd = Obstacle.lenght
-                Obstacle.YmapEnd = 1
+                Obstacle.XmapEnd = Obstacle.Xmap + Obstacle.lenght
+                Obstacle.YmapEnd = Obstacle.Ymap
+                Obstacle.Xlenght = Obstacle.lenght
+                Obstacle.Ylenght = 1
                 Obstacle.X = Obstacle.Xmap * casePxSide - casePxSide
                 Obstacle.Y = Obstacle.Ymap * casePxSide - casePxSide
                 for i = Obstacle.Xmap, Obstacle.Xmap + Obstacle.lenght-1 do
                     map[i][Obstacle.Ymap].state = Obstacle.name
                 end
             elseif Obstacle.dir == 2 then
-                Obstacle.XmapEnd = - Obstacle.lenght
-                Obstacle.YmapEnd = 1
+                Obstacle.XmapEnd = Obstacle.Xmap - Obstacle.lenght
+                Obstacle.YmapEnd = Obstacle.Ymap
+                Obstacle.Xlenght = - Obstacle.lenght
+                Obstacle.Ylenght = 1
                 Obstacle.X = Obstacle.Xmap * casePxSide 
                 Obstacle.Y = Obstacle.Ymap * casePxSide - casePxSide
                 for i = Obstacle.Xmap, Obstacle.Xmap - Obstacle.lenght+1, -1 do
                     map[i][Obstacle.Ymap].state = Obstacle.name
                 end
             elseif Obstacle.dir == 3 then
-                Obstacle.XmapEnd = 1
-                Obstacle.YmapEnd =  Obstacle.lenght
+                Obstacle.XmapEnd = Obstacle.Xmap
+                Obstacle.YmapEnd = Obstacle.Ymap + Obstacle.lenght
+                Obstacle.Xlenght = 1
+                Obstacle.Ylenght = Obstacle.lenght
                 Obstacle.X = Obstacle.Xmap * casePxSide - casePxSide
                 Obstacle.Y = Obstacle.Ymap * casePxSide - casePxSide
                 for i = Obstacle.Ymap, Obstacle.Ymap + Obstacle.lenght-1 do
                     map[Obstacle.Xmap][i].state = Obstacle.name
                 end
             elseif Obstacle.dir == 4 then
-                Obstacle.XmapEnd = 1
-                Obstacle.YmapEnd = - Obstacle.lenght
+                Obstacle.XmapEnd = Obstacle.Xmap
+                Obstacle.YmapEnd = Obstacle.Ymap - Obstacle.lenght
+                Obstacle.Xlenght = 1
+                Obstacle.Ylenght = - Obstacle.lenght
                 Obstacle.X = Obstacle.Xmap * casePxSide - casePxSide
                 Obstacle.Y = Obstacle.Ymap * casePxSide 
                 for i = Obstacle.Ymap, Obstacle.Ymap - Obstacle.lenght+1, -1 do
                     map[Obstacle.Xmap][i].state = Obstacle.name
                 end
             end
-            Obstacle.XEnd = Obstacle.XmapEnd * casePxSide 
-            Obstacle.YEnd = Obstacle.YmapEnd * casePxSide 
+
+            Obstacle.XEnd = Obstacle.Xlenght * casePxSide 
+            Obstacle.YEnd = Obstacle.Ylenght * casePxSide
         end
 
         Obstacle.draw = function() 
-            love.graphics.draw(Obstacle.image.src, Obstacle.X, Obstacle.Y,0,Obstacle.XmapEnd/Obstacle.image.scale, Obstacle.YmapEnd/Obstacle.image.scale)
+            love.graphics.draw(Obstacle.image.src, Obstacle.X, Obstacle.Y,0,Obstacle.Xlenght/Obstacle.image.scale, Obstacle.Ylenght/Obstacle.image.scale)
         end
 
         Obstacle.move = function(key, map)
@@ -67,10 +78,24 @@ obstacle.new = function(Xmap, Ymap, lenght, dir, image, name)
                 end
             end
             if (Obstacle.dir == 1 or Obstacle.dir == 2) and key == controls.left then
-                if Perso.XMap == Obstacle.Xmap+1 and Perso.YMap == Obstacle.Ymap then
+                if Perso.XMap == Obstacle.XmapEnd and Perso.YMap == Obstacle.Ymap then
                     Obstacle.Xmap = Obstacle.Xmap - 1
                     Obstacle.XmapEnd = Obstacle.XmapEnd - 1
-                    map[Obstacle.Xmap+1][Obstacle.Ymap].state = "vide"
+                    map[Obstacle.XmapEnd][Obstacle.Ymap].state = "vide"
+                end
+            end
+            if (Obstacle.dir == 3 or Obstacle.dir == 4) and key == controls.up then
+                if Perso.XMap == Obstacle.Xmap and Perso.YMap == Obstacle.Ymap-1 then
+                    Obstacle.Ymap = Obstacle.Ymap + 1
+                    Obstacle.YmapEnd = Obstacle.YmapEnd + 1
+                    map[Obstacle.Xmap][Obstacle.Ymap-1].state = "vide"
+                end
+            end
+            if (Obstacle.dir == 3 or Obstacle.dir == 4) and key == controls.down then
+                if Perso.XMap == Obstacle.Xmap and Perso.YMap == Obstacle.Ymap-1 then
+                    Obstacle.Ymap = Obstacle.Ymap - 1
+                    Obstacle.YmapEnd = Obstacle.YmapEnd - 1
+                    map[Obstacle.Xmap][Obstacle.YmapEnd-1].state = "vide"
                 end
             end
         end
@@ -79,3 +104,7 @@ end
 
 obstacle.move = function()
 end
+
+
+
+
