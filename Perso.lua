f9a1e0e501ec9e7532591061fde17676ff7cca98
ZeProf2C <mail@normanalie.fr>
2020-02-03 09:40:46 +0400

    1 03-02-2020

--- Perso.lua
+++ Perso.lua
@@ -18,51 +18,58 @@ function Perso.draw()
   love.graphics.draw(Perso.image.src,Perso.X,Perso.Y, math.rad(Perso.dir),0.15,0.15,Perso.image.width/2,Perso.image.height/2)
 end
 
+function Perso.move(dir)
+  if dir == "right" then
+    Perso.dir = 0
+    if Map[Perso.XMap + 1][Perso.YMap].busy == false then
+      Map[Perso.XMap][Perso.YMap].state = "vide"
+      Perso.XMap = Perso.XMap + 1
+      Map[Perso.XMap][Perso.YMap].state = "perso"
+    end
+  elseif dir == "left" then
+    Perso.dir = 180
+    if Map[Perso.XMap - 1][Perso.YMap].busy == false then
+      Map[Perso.XMap][Perso.YMap].state = "vide"
+      Perso.XMap = Perso.XMap - 1
+      Map[Perso.XMap][Perso.YMap].state = "perso"
+    end
+  elseif dir == "up" then
+    Perso.dir = 270
+    if Map[Perso.XMap][Perso.YMap - 1].busy == false then
+      Map[Perso.XMap][Perso.YMap].state = "vide"
+      Perso.YMap = Perso.YMap - 1
+      Map[Perso.XMap][Perso.YMap].state = "perso"
+    end
+  elseif dir == "down" then
+    Perso.dir = 90
+    if Map[Perso.XMap][Perso.YMap + 1].busy == false then
+      Map[Perso.XMap][Perso.YMap].state = "vide"
+      Perso.YMap = Perso.YMap + 1
+      Map[Perso.XMap][Perso.YMap].state = "perso"
+    end
+  else
+    return 1
+  end
+end
+
 function love.keypressed(key)
   uli.move(key, Map)
   nenenene.move(key, Map)
   obs.move(key, Map)
   if key == controls.right and Perso.XMap < caseCountWidth then
-    Perso.dir = 0
-    if Map[Perso.XMap + 1][Perso.YMap].busy == true then
-      obstacle.move(Map[Perso.XMap + 1][Perso.YMap].state, Perso.Xmap, Perso.Ymap,  1)
-    else
-      Map[Perso.XMap][Perso.YMap].state = "vide"
-      Perso.XMap = Perso.XMap + 1
-      Map[Perso.XMap][Perso.YMap].state = "perso"
-    end
+    Perso.move("right")
   end
+  
   if  key == controls.left and Perso.XMap > 1 then
-    Perso.dir = 180
-    if Map[Perso.XMap - 1][Perso.YMap].busy == true then
-      obstacle.move(Map[Perso.XMap - 1][Perso.YMap].state, Perso.Xmap, Perso.Ymap,  2)
-    else
-      Map[Perso.XMap][Perso.YMap].state = "vide"
-      Perso.XMap = Perso.XMap - 1
-      Map[Perso.XMap][Perso.YMap].state = "perso"
-    end
+    Perso.move("left")
   end
   
   if  key == controls.up and Perso.YMap > 1 then
-    Perso.dir = 270
-    if Map[Perso.XMap][Perso.YMap - 1].busy == true then
-      obstacle.move(Map[Perso.XMap][Perso.YMap - 1].state, Perso.Xmap, Perso.Ymap,  4)
-    else
-      Map[Perso.XMap][Perso.YMap].state = "vide"
-      Perso.YMap = Perso.YMap - 1
-      Map[Perso.XMap][Perso.YMap].state = "perso"
-    end
+    Perso.move("up")
   end
   
   if  key == controls.down and Perso.YMap < caseCountHeight then
-    Perso.dir = 90
-    if Map[Perso.XMap][Perso.YMap + 1].busy == true then
-      obstacle.move(Map[Perso.XMap][Perso.YMap + 1].state, Perso.Xmap, Perso.Ymap,  3)
-    else
-      Map[Perso.XMap][Perso.YMap].state = "vide"
-      Perso.YMap = Perso.YMap + 1
-      Map[Perso.XMap][Perso.YMap].state = "perso"
-    end
+    Perso.move("down")
   end
 
   if key==controls.quit then
