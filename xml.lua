cc50d7f280a8add241df2ce03113b871ce73d9d6
ZeProf2C <mail@normanalie.fr>
2020-05-20 14:09:29 +0200

    xml scores

--- xml.lua
+++ xml.lua
@@ -6,8 +6,20 @@ local parser = xml2lua.parser(handler)
 local controlsXml = xml2lua.loadFile("ressources/data/controls.xml")
 local scoresXml = xml2lua.loadFile("ressources/data/scores.xml")
 
+
 parser:parse(scoresXml)
 parser:parse(controlsXml)
 
 controls = handler.root.controls
-scores = handler.root.scores
+scores = handler.root.scores.game
+
+function addScore(value)
+    scoresFile = io.open("ressources/data/scores.xml", "w")
+    io.output(scoresFile)
+
+    scores[#scores+1] = {date = os.date(), score=value}
+
+    io.write(xml2lua.toXml(handler.root.scores, "scores"))
+    io.close(scoresFile)
+end
+
